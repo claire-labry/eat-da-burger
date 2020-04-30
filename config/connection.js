e37fcd33c63525c2e739fdf3d2e8f42d661397f1
claire-labry <clairestjlabry@gmail.com>
2020-04-30 14:42:09 -0500

    created schema and seeds files and created database

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 8080,
+    user: 'root',
+    password: 'PlantLady!3735',
+    database: 'burgers_db'
+});
+
+connection.connect(function(err){
+    if(err){
+        console.log('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name, devoured) VALUES ('Mushrooom Burger',true);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('McBurgy Burger', true);
