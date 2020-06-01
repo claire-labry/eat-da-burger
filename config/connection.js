b64454edbeb5948d5a5427f0ecd77c225a65233a
claire-labry <clairestjlabry@gmail.com>
2020-06-01 16:03:11 -0500

    deep clean of styling

--- config/connection.js
+++ config/connection.js
@@ -2,31 +2,30 @@ var mysql = require('mysql');
 require('dotenv').config();
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'PlantLady!3735',
-    database: 'burgers_db'
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'PlantLady!3735',
+  database: 'burgers_db',
 });
 
-if(process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else{
-    connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'host',
-        password: 'PlantLady!3735',
-        database: 'burgers_db'
-    })
-}
+// if(process.env.JAWSDB_URL){
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else{
+//     connection = mysql.createConnection({
+//         host: 'localhost',
+//         user: 'host',
+//         password: 'PlantLady!3735',
+//         database: 'burgers_db'
+//     })
+// }
 
-
-connection.connect(function(err){
-    if(err){
-        console.log('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
+connection.connect(function (err) {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
